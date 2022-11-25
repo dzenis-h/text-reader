8cc329f4d819d1f934be3583529428995298c203
Dzenis H <biggahd@Dzeniss-MacBook-Pro.local>
2022-11-25 13:00:41 +0100

    Initial commit

---
+++ data.js
@@ -0,0 +1,50 @@
+export default [
+  {
+    image: "./img/drink.jpg",
+    text: "I need a drink",
+  },
+  {
+    image: "./img/food.jpeg",
+    text: "It's time to eat",
+  },
+  {
+    image: "./img/sleep.jpg",
+    text: "I need to sleep",
+  },
+  {
+    image: "./img/cinema.jpg",
+    text: "Let's watch a movie",
+  },
+  {
+    image: "./img/happy.jpg",
+    text: "I'm so happy",
+  },
+  {
+    image: "./img/gym.jpg",
+    text: "I'm going to the GYM",
+  },
+  {
+    image: "./img/sad.jpg",
+    text: "I'm Sad",
+  },
+  {
+    image: "./img/alone.jpg",
+    text: "I'm lonely",
+  },
+  {
+    image: "./img/meditating.jpg",
+    text: "I'm need to relax",
+  },
+  {
+    image: "./img/depression.jpg",
+    text: "I feel depressed",
+  },
+  {
+    image: "./img/manic.jpg",
+    text: "I feel manic",
+  },
+  {
+    image: "./img/meds.jpg",
+    text: "It's time for my medication",
+  },
+];
